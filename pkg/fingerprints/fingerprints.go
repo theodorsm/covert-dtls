7d4deb1e1ed596348175c2865dc55688cdff54de
theodorsm <theodor@midtlien.com>
2024-06-12 15:03:22 +0200

    Workflow commit message with browser version

--- pkg/fingerprints/fingerprints.go
+++ pkg/fingerprints/fingerprints.go
@@ -3,7 +3,7 @@ package fingerprints
 //nolint:revive,unused
 type ClientHelloFingerprint string
 
-// These fingerprints are added automatically generated and added by the 'fingerprint' workflow
+// These fingerprints are automatically generated and added by the 'fingerprint' workflow
 // The first byte should correspond to the DTLS version in a handshake message
 const (
 	Mozilla_Firefox_125_0_1              ClientHelloFingerprint = "fefda62c8fe5497b56ad1e096f4294cf48c8fe97699406088833f3076ed35bb12b0200000010c02bc02fcca9cca8c00ac009c013c0140100006a00170000ff01000100000a00080006001d00170018000b000201000010001200100677656272746308632d776562727463000d0020001e040305030603020308040805080604010501060102010402050206020202001c00024000000e000b0008000700080001000200"                                                                                                                                                       //nolint:revive,stylecheck
