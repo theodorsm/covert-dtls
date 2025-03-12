f584cac3b4cbf2dff3fa1654bf14ed6815ac7136
theodorsm <theodor@midtlien.com>
2025-03-12 21:49:32 +0100

    Remove unused errors

--- pkg/utils/errors.go
+++ pkg/utils/errors.go
@@ -10,5 +10,4 @@ import (
 var (
 	errBufferTooSmall = &protocol.TemporaryError{Err: errors.New("buffer is too small")}                         //nolint:goerr113
 	errLengthMismatch = &protocol.InternalError{Err: errors.New("data length and declared length do not match")} //nolint:goerr113
-	errCookieTooLong  = &protocol.FatalError{Err: errors.New("cookie must not be longer then 255 bytes")}        //nolint:goerr113
 )
